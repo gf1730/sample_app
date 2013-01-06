d66ab3505faddf2cf1052385c31b9f199a3ac729
gf1730 <graeme.farmer@ventyx.abb.com>
2013-01-06 10:00:31 +0800

    Add full_title helper and refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,15 +6,21 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Home'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+                        :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit "/static_pages/home"
+      page.should_not have_selector('title',
+                                    :text => "| Home")
     end
   end
 
