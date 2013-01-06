763d4292f8e3479584c14cd0f942343e4b3e1ada
gf1730 <graeme.farmer@ventyx.abb.com>
2013-01-07 01:05:57 +0800

    Model users

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+
+  attr_accessible :email, :name, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :name,
+    presence: true, 
+    length: { maximum: 50 }
+
+  VALID_EMAIL_REGEXP = %r{(?i:\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z)}
+  validates :email, 
+    presence: true, 
+    uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEXP }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  before_save do |user| 
+    user.name = user.name.split.map(&:capitalize).join(' ')
+    user.email = email.downcase 
+  end
+  
+end
